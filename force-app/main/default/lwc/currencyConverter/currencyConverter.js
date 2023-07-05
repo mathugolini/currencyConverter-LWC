897e8010f369d5ab00362d5a6efc30340c89ac73
Matheus <mathugolini@gmail.com>
2023-07-04 23:40:08 -0300

    Import de API exchangerate.host

--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -1,17 +1,34 @@
 import { LightningElement } from 'lwc';
-import { countryCodeList } from 'c/countryCodeList'
+import {countryCodeList} from 'c/countryCodeList'
 import currencyConverterAssets from '@salesforce/resourceUrl/currencyConverterAssets'
-export default class CurrencyConverter extends LightningElement {
-
-    currencyImage = currencyConverterAssets + '/currencyConverterAssets/currency.svg'
-    countryList = countryCodeList
-    countryFrom = "USD"
-    countryTo = "AUD"
-
-    handleChange(event) {
-        const {name, value} = event.target
-        console.log("name", name)
-        console.log("value", value)
-        this[name] = value
+export default class CurrencyConverterApp extends LightningElement {
+  currencyImage = currencyConverterAssets +'/currencyConverterAssets/currency.svg'
+  countryList = countryCodeList
+  countryFrom = "USD"
+  countryTo = "AUD"
+  amount =''
+  result
+  error 
+  handleChange(event){
+    const {name, value} = event.target
+    console.log("name", name)
+    console.log("value", value)
+    this[name] = value
+  }
+  submitHandler(event){
+    event.preventDefault()
+    this.convert()
+  }
+  async convert(){
+    const API_URL = `https://api.exchangerate.host/convert?from=${this.countryFrom}&to=${this.countryTo}`
+    try{
+      const data = await fetch(API_URL)
+      const jsonData = await data.json()
+      this.result = (Number(this.amount) * jsonData.result).toFixed(2)
+      console.log(this.result)
+    } catch(error){
+      console.log(error)
+      this.error="An error occurred. Please try again..."
     }
+  }
 }
