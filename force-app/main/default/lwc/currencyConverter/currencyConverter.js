0c80ddf23fe506826dc4514bee1935a208e45f46
Matheus <mathugolini@gmail.com>
2023-07-04 22:22:06 -0300

    Adição de componentes lightning

--- force-app/main/default/lwc/currencyConverter/currencyConverter.js
+++ force-app/main/default/lwc/currencyConverter/currencyConverter.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import { countryCodeList } from 'c/countryCodeList'
+export default class CurrencyConverter extends LightningElement {
 
-export default class CurrencyConverter extends LightningElement {}
+    countryList = countryCodeList
+    countryFrom = "USD"
+    countryTo = "AUD"
+    
+    handleChange(event) {
+        const {name, value} = event.target
+        console.log("name", name)
+        console.log("value", value)
+    }
+}
